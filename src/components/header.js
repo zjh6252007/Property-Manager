eb240b7a5f4cccfcd07160b08929d314a0832010
unknown <zjh6252007@gmail.com>
2024-02-24 08:26:51 -0500

    update login page

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+const header = ()=>{
+<div>
+    header
+</div>
+}
+
+export default header
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,42 @@
+import { useState } from 'react'
+import './index.scss'
+
 const Login=()=>{
+    const[username,SetUsername] = useState('')
+    const[password,SetPassword] = useState('')
+
+    const handelSubmit = (e) =>{
+        e.preventDefault()
+        let error = false
+
+        if(!username.trim() || !password.trim){
+            error = true
+        }
+        if(!error){
+            console.log('success',{username,password})
+        }
+    }
     return(
-        <div>
-            Login
+        <div className='box'>
+            <form className='form' onSubmit={handelSubmit}>
+                <h2>Sign in</h2>
+                <div className='inputBox'>
+                    <input type="text" value={username} required="required" onChange={(e)=>SetUsername(e.target.value)}/>
+                    <span>Username</span>
+                    <i></i>
+
+                </div>
+                <div className='inputBox'>
+                    <input type="password" value={password} required="required" onChange={(e)=>SetPassword(e.target.value)}/>
+                    <span>Password </span>
+                    <i></i>
+                </div>
+                <div className="links">
+                    <a href="#">Forgot Password</a>
+                    <a href="#">Sign Up</a>
+                </div>
+                <input type="submit" value="Login"></input>
+            </form>
         </div>
     )
 }
