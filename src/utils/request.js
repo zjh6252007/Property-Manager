75f6288fcaf3d40e9352cacbfdea8291e5ae0644
Jay <zjh6252007@gmail.com>
2024-04-02 18:32:00 -0400

    Update Axios interceptor

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,12 @@
 import axios from "axios"
-import { getToken } from "./index"
+import { getToken,deleteToken } from "./index"
+import { createBrowserHistory } from "@remix-run/router"
 const request = axios.create({
     baseURL:'http://localhost:8080',
     timeout: 5000
 })
 
+const history = createBrowserHistory();
 request.interceptors.request.use((config)=>{
     const token = getToken()
     if(token){
@@ -18,6 +20,10 @@ request.interceptors.request.use((config)=>{
 request.interceptors.response.use((response)=>{
     return response.data
 },(error)=>{
+    if(error.response && error.response.status === 401){
+        deleteToken()
+        history.push('/login')
+    }
     return Promise.reject(error)
 })
 
