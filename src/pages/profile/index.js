3947c6cde1fdd57f77608dc2c3cdc8a686942485
Jay <zjh6252007@gmail.com>
2024-04-08 05:47:35 -0400

    Add rules to change password function

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,24 +1,34 @@
 import './index.scss'
-import { Button,Modal,Form,Input} from 'antd';
+import { Button,Modal,Form,Input,message} from 'antd';
 import { useForm } from 'antd/es/form/Form';
 import { useState } from 'react';
 import { changePwd } from '../../store/modules/user';
+import { useSelector } from 'react-redux';
 const Profile =()=>{
     const [form] = useForm()
     const [isVisible,SetIsVisible] = useState(false)
-
+    const [pwdInfo,SetPwdInfo] = useState('')
+    const email = useSelector(state=>state.user.userInfo.email)
+    const username = useSelector(state=>state.user.userInfo.username)
     const showModal=()=>{
         SetIsVisible(true)
     }
 
     const handelCancel=()=>{
+        SetPwdInfo('')
+        form.resetFields()
         SetIsVisible(false)
     }
 
     const handelOk=async()=>{
         const values = await form.validateFields()
         const response = await changePwd(values)
-        console.log(response)
+        SetPwdInfo(response.message)
+        if(response.code === 0){
+        message.success('change password succes')
+        SetPwdInfo('')
+        SetIsVisible(false)
+        form.resetFields()}
     }
     return (
         <div className="profile">
@@ -27,17 +37,17 @@ const Profile =()=>{
             </div>
             <div className='profile-box'>
                 <div className='accordion-item'>
-                    <h5 className='info-title'>Name</h5>
+                    <h5 className='info-title'>User Name</h5>
                     <div className='info-area'>
-                        <p className='info-text'>Nameinfo</p>
-                        <Button className="info-button" type="primary" size='large'>Edit</Button>
+                        <p className='info-text'>{username}</p>
+                        
                     </div>
                 </div>
 
                 <div className='accordion-item'>
                     <h5 className='info-title'>Email</h5>
                     <div className='info-area'>
-                        <p className='info-text'>Emailinfo</p>
+                        <p className='info-text'>{email}</p>
                         <Button className="info-button" type="primary" size='large'>Edit</Button>
                     </div>
                 </div>
@@ -67,10 +77,37 @@ const Profile =()=>{
                 <Form.Item
                 label="New Password"
                 name="new_pwd"
-                rules={[{ required: true, message: 'Please Enter New Password' }]}
+                rules={[{ required: true, message: 'Please Enter New Password' },
+                ()=>({
+                    validator(_,value){
+                        if(!value || (/[A-Za-z]/.test(value) && /\d/.test(value)))
+                        {
+                            return Promise.resolve()
+                        }
+                        return Promise.reject(new Error('Password must contain at least one letter and one number!'))
+                    }
+                })]}
                 >
                 <Input.Password />
                 </Form.Item>
+
+                <Form.Item
+                label="Confirm Password"
+                name="confirm_pwd"
+                dependencies={['new_pwd']}
+                rules={[{ required: true, message: 'Please Enter New Password' },
+                ({getFieldValue})=>({
+                    validator(_,value){
+                        if(!value || getFieldValue('new_pwd') === value){ //Confirm password must equal to new password
+                            return Promise.resolve()
+                        }
+                        return Promise.reject(new Error('Password does not match'))
+                    }
+                })]}
+                >
+                <Input.Password />
+                </Form.Item>
+                <li style={{color:'red',position:'relative',left:'30px'}}>{pwdInfo}</li>
                 </Form>
             </Modal>
         </div>
