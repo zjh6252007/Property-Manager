023ea9d75d167b467c5f8af36a437f5e132c5174
Jay <zjh6252007@gmail.com>
2024-05-01 16:29:50 -0400

    .

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { getToken,deleteToken } from "./index"
 const request = axios.create({
-    baseURL:'http://54.172.6.160:8080',
+    baseURL:'https://54.172.6.160:443',
     timeout: 5000
 })
 
